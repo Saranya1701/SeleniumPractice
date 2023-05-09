3cec1ec2a9666378f47e44cd38606b04a3f9c3e3
Saranya1701 <saranyasarnya1701@gmail.com>
2023-05-09 15:03:09 +0530

    new branch created

--- src/main/java/seleniumPractice/mavenProject/sample.java
+++ src/main/java/seleniumPractice/mavenProject/sample.java
@@ -5,7 +5,10 @@ public class sample {
 	public static void main(String[] args) {
 System.out.println("Hello");
 sample s= new sample();
-
+System.out.print("hi saranya");
+System.out.print("hi saranya");
+System.out.print("hi saranya");
+System.out.print("hi saranya");
 	}
 }
 
